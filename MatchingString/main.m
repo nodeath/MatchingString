a5f29995bff5f2197b1176dfc7657f312a1b4e6d
Shawn Hung <shawnhung@paganiniplus.com>
2015-10-08 11:34:35 +0800

    Initial Commit

---
+++ MatchingString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MatchingString
+//
+//  Created by Shawn Hung on 10/8/15.
+//  Copyright © 2015 Paganini Plus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
